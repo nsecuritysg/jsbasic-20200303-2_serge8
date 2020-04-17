f547d40022243c1fef08d41960a0782d371377db
Sergey Nepsha <nsecuritysg@nxt.ru>
2020-04-18 02:38:56 +0300

    Создана функция, находящая min/max в строке

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,20 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+    let a = '';
+    let arr = [];
+    for (let i = 0; i < str.length+1; i++) {
+        if (isFinite(+str[i]) ||
+            str[i] == '.' ||
+            str[i] == '-') {
+                a += str[i];
+            } else if (+a && a !== '') {
+                arr.push(+a);
+                a = '';          
+            }
+    }
+    return {
+        min: Math.min(...arr),
+        max: Math.max(...arr)
+    };
 }
